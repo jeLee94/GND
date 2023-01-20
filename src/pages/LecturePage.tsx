c3ddcff9a878700b8ad7403d49e94037c6660205
Hoesu Ji <newasborn@naver.com>
2023-01-20 20:01:26 +0900

    design: 강의 페이지 UI 작업

--- src/pages/LecturePage.tsx
+++ src/pages/LecturePage.tsx
@@ -1,6 +1,57 @@
 //유튜브 API 활용한 강의 페이지
+
+import styled from 'styled-components';
+import ReactPlayer from 'react-player';
+
 const LecturePage = () => {
-  return <div>LecturePage</div>;
+  return (
+    <StContainer>
+      <StLectureHeader>
+        <StLectureTitle>혼자서도 마스터하는 React</StLectureTitle>
+        <StBackButton>Back to Course</StBackButton>
+      </StLectureHeader>
+      <ReactPlayer
+        url={
+          'https://www.youtube.com/watch?v=t3M6toIflyQ&list=PLv2d7VI9OotSn1ThdDeqvBx8QuRSd01qv&index=1'
+        }
+        width='900px'
+        height='510px'
+        muted={true}
+        controls={true}
+      />
+    </StContainer>
+  );
 };
 
 export default LecturePage;
+
+const StContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StLectureHeader = styled.div`
+  width: 900px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const StLectureTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+const StBackButton = styled.button`
+  width: 150px;
+  height: 35px;
+  border: none;
+  border-radius: 5px;
+  background-color: #76c2b6;
+  color: #fff;
+  cursor: pointer;
+`;
