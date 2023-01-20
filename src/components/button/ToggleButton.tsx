6089c7bb139d90621e6ab7fe25c81622609f7493
lth97 <0ne0rzer0@naver.com>
2023-01-20 21:43:07 +0900

    feat : 강의 설명, 강의 리스트 화면 추가

---
+++ src/components/button/ToggleButton.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const handleToggle = () => {
+  alert("강의 목록 보여주기 버튼");
+};
+const ToggleButton = () => {
+  return (
+    <>
+      <ToggleBtn onClick={handleToggle}></ToggleBtn>
+    </>
+  );
+};
+
+export default ToggleButton;
+
+const ToggleBtn = styled.button`
+  width: 30px;
+  height: 30px;
+  margin-left: 600px;
+  border-radius: 20px;
+  background-color: #3b615b;
+`;
