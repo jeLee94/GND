114aa344b8de61d48c561be55347c6d43a62f0d4
u_ju <89282724+Sangwoo950@users.noreply.github.com>
2023-01-23 22:31:40 +0900

    [feat] 메인페이지 초안
    파베 연결 전
    슬라이드 배너 완성본.

---
+++ src/pages/Myslide.jsx
@@ -0,0 +1,31 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay} from 'swiper';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css';
+import './Myslide.css';
+import { on } from 'events';
+// import styled from 'styled-components';
+
+const Slide = () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Autoplay]}
+      pagination={{ clickable: true }}
+      navigation
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      autoplay={{ "delay:": 1000 }}
+      loop={true}
+    >
+      <SwiperSlide className='slide'>Slide 1</SwiperSlide>
+      <SwiperSlide className='slide'>Slide 2</SwiperSlide>
+      <SwiperSlide className='slide'>Slide 3</SwiperSlide>
+      <SwiperSlide className='slide'>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
+export default Slide;
