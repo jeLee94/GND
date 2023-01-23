693756004ed5a785f57b0daaf608beb4b9061836
이정은 <2019152052@kpu.ac.kr>
2023-01-23 10:02:07 +0900

    feat: 파이어베이스 데이터베이스 설정 및 데이터 저장

--- src/pages/LecturePage.tsx
+++ src/pages/LecturePage.tsx
@@ -1,7 +1,7 @@
 //유튜브 API 활용한 강의 페이지
 
 import styled from 'styled-components';
-import ReactPlayer from 'react-player';
+// import ReactPlayer from 'react-player';
 
 const LecturePage = () => {
   return (
@@ -10,7 +10,7 @@ const LecturePage = () => {
         <LectureTitle>혼자서도 마스터하는 React</LectureTitle>
         <BackButton>Back to Course</BackButton>
       </LectureHeader>
-      <ReactPlayer
+      {/* <ReactPlayer
         url={
           'https://www.youtube.com/watch?v=t3M6toIflyQ&list=PLv2d7VI9OotSn1ThdDeqvBx8QuRSd01qv&index=1'
         }
@@ -18,7 +18,7 @@ const LecturePage = () => {
         height='510px'
         muted={true}
         controls={true}
-      />
+      /> */}
     </Container>
   );
 };
