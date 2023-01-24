536a9f43c2f96dd0c9cc7f0e13bf14bbd3727aff
lth97 <0ne0rzer0@naver.com>
2023-01-24 18:34:11 +0900

    feat : 배너 이미지 예시

--- src/pages/Myslide.tsx
+++ src/pages/Myslide.tsx
@@ -1,10 +1,12 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay } from 'swiper';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import 'swiper/css';
-import './Myslide.css';
-import { on } from 'events';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css";
+import "./Myslide.css";
+import { on } from "events";
+import reactnativebanner from "../util/img/reactnativethumbnail.png";
+import reactbanner from "../util/img/react1.png";
 // import styled from 'styled-components';
 
 const Slide = () => {
@@ -16,15 +18,23 @@ const Slide = () => {
       navigation
       spaceBetween={50}
       slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
       // autoplay={{ delay: 1000 }}
       loop={true}
     >
-      <SwiperSlide className='slide'>Slide 1</SwiperSlide>
-      <SwiperSlide className='slide'>Slide 2</SwiperSlide>
-      <SwiperSlide className='slide'>Slide 3</SwiperSlide>
-      <SwiperSlide className='slide'>Slide 4</SwiperSlide>
+      <SwiperSlide className="slide">
+        <a href="http://localhost:3000/lecture/scZI19SE0_4">
+          <img width={2000} src={reactnativebanner} />
+        </a>
+      </SwiperSlide>
+      <SwiperSlide className="slide">
+        <a href="http://localhost:3000/lecture/Qb8Oiy8i9IY">
+          <img width={1000} height={500} src={reactbanner} />
+        </a>
+      </SwiperSlide>
+      <SwiperSlide className="slide">Slide 3</SwiperSlide>
+      <SwiperSlide className="slide">Slide 4</SwiperSlide>
     </Swiper>
   );
 };
