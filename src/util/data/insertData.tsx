7fa7bcf49c5ffe7337ca40bc375b788bb37cdea2
이정은 <2019152052@kpu.ac.kr>
2023-01-27 20:00:37 +0900

    etc: insertData파일 주석 수정

--- src/util/data/insertData.tsx
+++ src/util/data/insertData.tsx
@@ -22,7 +22,7 @@ const InsertData = () => {
     e.preventDefault();
     axios
       .get(
-        //단일 영상용
+        //단일 영상용 q=검색하고 싶은 단어
         'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=cs전공지식&order=relevance&type=video&key=AIzaSyBFGvK1hpr4U1u6BArtxPAwiUJ90Qt99x4'
         //플레이리스트용
         // 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=리액트&order=relevance&type=playlist&key=AIzaSyBFGvK1hpr4U1u6BArtxPAwiUJ90Qt99x4'
@@ -42,6 +42,7 @@ const InsertData = () => {
         playlist.map((i: any, idx) => {
           console.log(i, idx);
           if (i?.id?.playlistId) {
+            //플레이리스트일때
             addDoc(collection(dbService, 'CLASS'), {
               category: 'react',
               channelId: i?.snippet?.channelId,
@@ -51,6 +52,7 @@ const InsertData = () => {
               thumbnail: i?.snippet?.thumbnails.high.url,
             });
           } else if (i?.id?.videoId || i?.id) {
+            //단일 영상일떄
             addDoc(collection(dbService, 'CLASS'), {
               category: 'cs',
               channelId: i?.snippet?.channelId,
@@ -85,7 +87,6 @@ const InsertData = () => {
   const getPlaylistId = async () => {
     await promise.then((data: any) => {
       setPlaylistIdList(data);
-      // setPlaylistIdList(data.map((d: any) => d.playlistId));
     });
   };
   console.log('d', playlistIdList);
@@ -152,7 +153,7 @@ const InsertData = () => {
             updateDoc(docRef, {
               videotitle: arrayUnion(i?.snippet?.title),
               description: arrayUnion(i?.snippet?.description ?? '없음'),
-              videoId: arrayUnion(i?.id?.videoId),
+              videoId: arrayUnion(i?.id?.videoId), //내배캠용은 인자 다름 주의
               thumbnail: arrayUnion(i?.snippet?.thumbnails?.high.url ?? '없음'),
             })
               .then(() => console.log('완료!'))
